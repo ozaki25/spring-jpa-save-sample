80bdf52e1395b66eebce12662976cbe2abae8624
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-20 08:36:34 +0900

    add comment

--- src/main/java/com/example/demo/TelRepository.java
+++ src/main/java/com/example/demo/TelRepository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TelRepository extends JpaRepository<TelData, Long> {
   // 本当は戻り値の型をList<TelBean>にしたいがTelBeanはEntityでないから指定できず仕方なくList<Object[]>で返す
+  // 以下の記事を参考に実装
+  // https://www.ne.jp/asahi/hishidama/home/tech/java/spring/boot/jpa/jpql_join.html
   @Query(value = "SELECT d.id,d.user_name,d.tel_no,d.mail_addr,g.group_name FROM tel_data d LEFT JOIN tel_group g ON d.group_id = g.group_id", nativeQuery = true)
   public List<Object[]> findAllData();
 }
